8c42ddc2b0afa2cab360fb39161991ab188eb371
Gokulcasper <ronitcasper215@gmail.com>
2024-07-23 06:56:59 +0530

    Commit Find & FindIndex Methods

---
+++ index.js
@@ -0,0 +1,39 @@
+//-----Include Methods-------
+// let myArray= ["Apple","Carrot","Orange"];
+// let res = myArray.includes("Banana");
+// if(res){
+//     console.log("Happy!");
+// }else{
+//     console.log("Sad!")
+// }
+
+//------Find-------
+// let person = [
+//     {
+//         name:"Gokul",
+//         number:1
+//     },
+//     {
+//         name:"Raj",
+//         number:2
+//     },
+//     {
+//         name:"Ajay",
+//         number:3
+//     }
+// ]
+// // let res = person.find((person)=>{
+// //     return person.name === "Gokul";
+// // })
+// let res1= person.find(findPerson);
+// function findPerson(person){
+//     return person.name === "Raj"; 
+// }
+// console.log(res1);
+
+//------FindIndex--------
+// let number=[10,20,30,40];
+// let res = number.findIndex((num)=>{
+//     return num > 10;
+// })
+// console.log(res)
