47de42a5fd3cb822533fbf98414e69c2af7665b7
Gokulcasper <ronitcasper215@gmail.com>
2024-07-23 07:50:49 +0530

    Commit Adding Elements

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-//-----Include Methods-------
+// ---------------- FINDING  ELEMENTS -----------------
+
+// -----Include Methods-------
 // let myArray= ["Apple","Carrot","Orange"];
 // let res = myArray.includes("Banana");
+// console.log(res) //False
 // if(res){
 //     console.log("Happy!");
 // }else{
@@ -22,9 +25,10 @@
 //         number:3
 //     }
 // ]
-// // let res = person.find((person)=>{
-// //     return person.name === "Gokul";
-// // })
+// let res = person.find((person)=>{
+//     return person.name === "Gokul";
+// })
+// console.log(res)
 // let res1= person.find(findPerson);
 // function findPerson(person){
 //     return person.name === "Raj"; 
@@ -37,8 +41,12 @@
 //     return num > 10;
 // })
 // console.log(res)
+// let result = number.findIndex((value)=>{
+// return value === 30;
+// })
+// console.log(result)
 
-//------IndexOf-----------
+// ------IndexOf-----------
 // let names = ["Gokul","Raj","Bhuvi"];
 // let res = names.indexOf("Raj");
 // console.log(res)
@@ -54,6 +62,31 @@
 // }
 
 //-------LastIndexOf-------------
-// let values = [10,20,50,40,50,60,30,20,70,10,90];
+// let values = [10,20,50,40,50,60,30,20,70,10,20,50];
 // let res = values.lastIndexOf(20);
 // console.log(res)
+//
+
+
+// ---------------- ADDING ELEMENTS -------------------
+
+//-------Push--------
+// let number = [1,2,3,4,5];
+// let value = number.push(6,7);
+// console.log(number,value);
+
+//------Unshift-------
+// let number = [1,2,3,4,5];
+// number.unshift(0);
+// number.unshift(1,-1);
+// let total = number.unshift(2);
+// console.log(number);
+// console.log(total);
+
+//------Splice--------
+// let number = [5,4,3,9,8];
+// let deleted = number.splice(2,1,7,6);
+// console.log(deleted)
+// console.log(number) 
+
+// ------------------ REMOVING ELEMENTS ---------------------
